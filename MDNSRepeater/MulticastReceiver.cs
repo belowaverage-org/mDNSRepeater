b91ffc324ac0d7168da033ccb0dac2407537e82b
Dylan Bickerstaff <dylan@belowaverage.org>
2023-02-11 23:06:35 -0500

    Fixed loopback.

--- MDNSRepeater/MulticastReceiver.cs
+++ MDNSRepeater/MulticastReceiver.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 
 namespace MDNSRepeater
@@ -8,11 +9,12 @@ namespace MDNSRepeater
         bool Disposing = false;
         UdpClient Client;
         IPEndPoint Endpoint = new IPEndPoint(IPAddress.Any, 5353);
+        IPAddress[] SelfAddresses;
         internal MulticastReceiver()
         {
             Shared.Log("Setting up MCR...");
+            ListAdapterIPs();
             Client = new UdpClient();
-            Client.MulticastLoopback = false;
             Client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
             Client.Client.Bind(Endpoint);
             Client.JoinMulticastGroup(IPAddress.Parse("224.0.0.251"));
@@ -24,9 +26,27 @@ namespace MDNSRepeater
             while (!Disposing)
             {
                 byte[] datagram = Client.Receive(ref Endpoint);
-                Shared.Log("MCR <- " + Endpoint.ToString());
-                Program.UnicastSender.Send(datagram);
+                if (!SelfAddresses.Contains(Endpoint.Address))
+                {
+                    Shared.Log("MCR <- " + Endpoint.ToString());
+                    Program.UnicastSender.Send(datagram);
+                }
             }
+        }
+        void ListAdapterIPs()
+        {
+            Shared.Log("Enumerating assigned IPs...");
+            List<IPAddress> ips = new List<IPAddress>();
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface inter in interfaces)
+            {
+                foreach (UnicastIPAddressInformation ip in inter.GetIPProperties().UnicastAddresses)
+                {
+                    Shared.Log("IP found: " + ip.Address.ToString() + ".");
+                    ips.Add(ip.Address);
+                }
+            }
+            SelfAddresses = ips.ToArray();
         }
         public void Dispose()
         {
